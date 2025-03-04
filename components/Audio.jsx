313b6da2a6bad8a823a5af59de68ae1e74018242
zzenghui <zhaozenghui@testdaily.cn>
2025-03-04 19:01:14 +0800

    init

---
+++ components/Audio.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import SpeechRecognition, {
+  useSpeechRecognition,
+} from "react-speech-recognition";
+
+const Dictaphone = () => {
+  const {
+    transcript,
+    listening,
+    resetTranscript,
+    browserSupportsSpeechRecognition,
+    isMicrophoneAvailable,
+  } = useSpeechRecognition();
+
+  // 用户拒绝使用麦克风
+  if (!isMicrophoneAvailable) {
+    alert("请打开权限");
+  }
+  //   浏览器不支持
+  if (!browserSupportsSpeechRecognition) {
+    return <span>Browser doesn't support speech recognition.</span>;
+  }
+  const start = () => {
+    SpeechRecognition.startListening({ continuous: true, language: "zh-CN" });
+  };
+  const stop = () => {
+    SpeechRecognition.stopListening();
+  };
+  const reset = () => {
+    resetTranscript();
+  };
+  return (
+    <div>
+      <p>Microphone: {listening ? "on" : "off"}</p>
+      <button onClick={() => start()}>Start</button>
+      <button onClick={() => stop()}>Stop</button>
+      <button onClick={reset}>Reset</button>
+      <p>{transcript}</p>
+    </div>
+  );
+};
+export default Dictaphone;
